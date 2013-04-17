498d37fad44c367e588bba83ea721c6a6afcbbef
Prashant Agarwal <pras2309@yahoo.com>
2013-04-18 00:16:13 +0530

    Added pages about us, privacy, terms, blog

---
+++ general/views.py
@@ -0,0 +1,20 @@
+# Create your views here.
+# Create your views here.
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from customuser.models import *
+
+@login_required
+def aboutus(request):
+    return render(request, 'general/aboutus.html', {'user_info':request.user}) 
+
+def terms(request):
+    return render(request, 'general/terms.html', {'user_info':request.user})
+
+def privacy(request):
+    return render(request, 'general/privacy.html', {'user_info':request.user})
+
+def blog(request):
+    return render(request, 'general/blog.html', {'user_info':request.user})
